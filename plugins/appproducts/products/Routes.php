ac4113738d7e02ec7e9c0c4abeda2fdb2afd1bb1
erdwell <erdwell@gmail.com>
2020-07-21 00:46:05 -0500

    cambio en route por url

--- plugins/appproducts/products/Routes.php
+++ plugins/appproducts/products/Routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Routing\Controller;
+
 Route::prefix('/api/v1/products') -> group(function(){
   Route::get('/{type}/{val}/{limit?}', 'AppProducts\Products\Controllers\Products@FindProducts');
 });
