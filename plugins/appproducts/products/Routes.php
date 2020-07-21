7cdf26db0aa49c2cb071d4e065d2049ff7e91514
erdwell <erdwell@gmail.com>
2020-07-21 11:46:58 -0500

    rollback para creación de productos erso

--- plugins/appproducts/products/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Routing\Controller;
-
-Route::prefix('/api/v1/products') -> group(function(){
-  Route::get('/{type}/{val}/{limit?}', 'AppProducts\Products\Controllers\Products@FindProducts');
-});
--- plugins/appshopping/orders/lang/en/lang.php
+++
@@ -1,6 +0,0 @@
-<?php return [
-    'plugin' => [
-        'name' => 'Orders',
-        'description' => ''
-    ]
-];
