dbff7d9f77599d66337961d65688866708c8e07b
erdwell <erdwell@gmail.com>
2020-07-24 04:31:52 -0500

    cambio de espacios en el archivo routes.php

--- plugins/loftonti/erso/routes.php
+++ plugins/loftonti/erso/routes.php
@@ -1,10 +1,8 @@
 <?php
 
   Route::prefix('api/v1') -> group(function(){
-    
     Route::get('search-car/{car}', 'Loftonti\Erso\Controllers\ProductsApiController@serachCars');
     Route::get('get-models/{car}', 'Loftonti\Erso\Controllers\ProductsApiController@getModels');
     Route::get('search-product-category-model/{model}/{category}', 'Loftonti\Erso\Controllers\ProductsApiController@SearchProductCategoryModel');
     Route::get('general-search-products/{data}', 'Loftonti\Erso\Controllers\ProductsApiController@GeneralSearchProduct');
-
   });
