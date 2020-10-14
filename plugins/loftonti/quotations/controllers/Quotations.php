cee26ca20cd99f09bc6711b628c939ec17e74b5c
MKTSTF05 <MKTSTF05@Lofton.com>
2020-10-14 16:21:52 -0500

    implementación del basepath para la url del logotipo de la ctización

--- plugins/loftonti/quotations/controllers/Quotations.php
+++ plugins/loftonti/quotations/controllers/Quotations.php
@@ -190,7 +190,7 @@ class Quotations extends Controller
             $data = [
                 $quotation,
                 $branch,
-                'download',
+                'stream',
                 Carbon::now() -> addMinutes(3) -> format('Y-m-d H:i:s')
             ];
             $data = Crypt::encryptString(json_encode($data));
