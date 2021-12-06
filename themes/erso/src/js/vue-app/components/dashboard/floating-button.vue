3191a9706f75f05cd215f95c81e13aef23c5ab00
Uriel Miranda <urielnovoa@gmail.com>
2021-12-06 13:04:04 -0600

    replace icon whats

--- themes/erso/src/js/vue-app/components/dashboard/floating-button.vue
+++ themes/erso/src/js/vue-app/components/dashboard/floating-button.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
    div
-    div.floating-button(@click.prevent="isActive = !isActive" :class="{'square': isActive }") 
-        i.fas.fa-phone-alt(:class="{'padd show':isActive}")
+    div.floating-button(@click.prevent="isActive = !isActive" :class="{'square reverse': isActive }") 
+        img(src="/themes/erso/assets/img/logo2.png", alt="alt" :class="{'hide': isActive }")
+        span.title.hide(:class="{'show': isActive }") WhatsApp 
+            img(src="/themes/erso/assets/img/logo2.png", alt="alt" :class="{'hide': !isActive }")
         a.fab-child(@click="openWhats('https://wa.me/525527358877')" :class="{'show': isActive }") Coacalco
         a.fab-child(@click="openWhats('https://wa.me/525549828827')" :class="{'show': isActive }") Tlalnepantla
         a.fab-child(@click="openWhats('https://wa.me/525546105175')" :class="{'show': isActive }") Cuautitlán Izcalli
@@ -17,18 +19,36 @@ export default {
     },
     methods: {
         openWhats(phone) {
-            window.open(phone, "_blank")
+            window.open(phone, "_blank");
         },
     },
 };
 </script>
 
 <style lang="sass">
+.hide
+    display: none
+
+.title
+    display: none
+    padding: 10px
+    font-weight: bold
+    height: 40px
+    background-color: #40c351
+    color: #FFF
+    width: 100%
+    display: flex
+    justify-content: center
+    border-radius: 10px 10px 0 0
+    img
+        margin-left:5px
+
+
 .floating-button
     background-color: #40c351
+    font-weight: bold
     width: 50px
     height: 50px
-    border: 4px solid white
     border-radius: 50%
     position: fixed
     right: 7%
@@ -47,6 +67,8 @@ export default {
     animation-iteration-count: 1
     animation-fill-mode: none
     animation-play-state: running
+    img
+        width:20px
     @media screen and ( min-width: 768px )
         right: 30px
         bottom: 10%
@@ -60,11 +82,12 @@ export default {
     bottom: 13%
     display: none
 .fab-child
-    border: 2px solid white
-    color: #40c351
+    border-top: 2px solid #f4f4f4
+    color: #000 !important
+    padding: 10px
+    width: 100%
     display: none
     color: white
-    border: none
     justify-content: center
     align-items: center
     animation-name: easyin
@@ -76,7 +99,7 @@ export default {
     animation-fill-mode: none
     animation-play-state: running
 .show
-    display: flex
+    display: flex !important
     animation-name: show
     animation-duration: .02s
     animation-delay: 0
@@ -89,10 +112,11 @@ export default {
     display: none
 .square
     display: flex
-    padding: 10px
+    color: #000
     flex-direction: column
-    width: 200px
-    height: 100px
+    width: 250px
+    height: auto
+    background-color: white
     border-radius: 10px
     animation-name: zoomin
     animation-duration: .02s
@@ -104,45 +128,44 @@ export default {
     animation-play-state: running
 .padd
     margin-top: 5px
+.reverse
+    flex-direcion: row-reverse !important
 @keyframes show
     0%
-        opacity: 0 
+        opacity: 0
     100%
         opacity: 1
-@keyframes zoomin 
-  0% 
+@keyframes zoomin
+  0%
+    background-color: #40c351
     width: 50px
-    height: 50px
-  50% 
+  50%
+    background-color: white
     width: 150px
-    height: 80px
-  
-  100% 
-    width: 200px
-    height: 100px
-@keyframes zoomout 
-  0% 
-    width: 200px
-    height: 100px
-    border-radius: 10px 
 
-  50% 
+  100%
+    background-color: white
+    width: 200px
+@keyframes zoomout
+  0%
+    width: 200px
+    border-radius: 10px
+
+  50%
     width: 150px
-    height: 80px
-    border-radius: 10px 
-  
-  100% 
+    background-color: #40c351
+    border-radius: 10px
+
+  100%
     width: 50px
-    height: 50px
     background-color: #40c351
     border-radius: 50%
 
-@keyframes easyin 
+@keyframes easyin
     0%
         display: none
         opacity: 0
     100%
         opacity: 1
         display: block
-
 </style>
